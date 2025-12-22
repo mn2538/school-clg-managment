a606ce32b4e842a701c412cdf1234677d5f535c6
Mohan <mohansaikrishna258@gmail.com>
2025-12-22 22:30:33 +0530

    Add frontend source code (remove accidental submodule)

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid, Card, CardContent, StatCard } from '@mui/material';
+
+export const Dashboard = () => {
+
+    <Grid container spacing={3}>
+        
+    </Grid>
+
+}
