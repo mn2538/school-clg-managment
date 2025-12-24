15b5662ac4447dbe5150a9a6c8fc0cb76ce8832b
Mohan <mohansaikrishna258@gmail.com>
2025-12-24 11:48:59 +0530

    new things-dashboard|goodmorning

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Grid, Card, CardContent, StatCard } from '@mui/material';
+import { Grid, Card, CardContent, StatCard, Typography } from '@mui/material';
+import {GoodMorning} from '../components/GoodMorning';
 
 export const Dashboard = () => {
-
+return (
     <Grid container spacing={3}>
-        
+        <GoodMorning name="Mohan"/>
     </Grid>
-
+)
 }
