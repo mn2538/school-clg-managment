dfa131fd37228909362e5257cd802ed1a4c05ca9
mn2538 <mohansaikrishna258@gmail.com>
2025-12-26 11:16:27 +0000

    marks context - used only in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Grid, Card, CardContent, StatCard, Typography } from '@mui/material';
-import {GoodMorning} from '../components/GoodMorning';
+import { Grid } from "@mui/material";
+import { GoodMorning } from "../components/GoodMorning";
+import { MarksCard } from "../components/MarksCard";
 
 export const Dashboard = () => {
-return (
-    <Grid container spacing={3}>
-        <GoodMorning name="Mohan"/>
+  return (
+    <Grid container spacing={3} sx={{ padding: 3 }}>
+      <Grid item xs={12} sx={{ width: "100%" }}>
+        <GoodMorning name="Mohan" />
+      </Grid>
+
+      <Grid item xs={12}>
+        <MarksCard />
+      </Grid>
     </Grid>
-)
-}
+  );
+};
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,34 +1,37 @@
 // src/routes/AppRoutes.jsx
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { AuthPage } from "../pages/AuthPage"; 
-import {MarksPage} from "../pages/MarksPage";
-import {Dashboard} from "../pages/Dashboard";
+import { AuthPage } from "../pages/AuthPage";
+import { MarksPage } from "../pages/MarksPage";
+import { Dashboard } from "../pages/Dashboard";
 import ProtectedRoute from "../components/ProtectedRoute";
+import { MarksProvider } from "../context/MarksContext";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<AuthPage />} />
+    <MarksProvider>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
 
-      <Route
-        path="/markspage"
-        element={
-          <ProtectedRoute>
-            <MarksPage />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/markspage"
+          element={
+            <ProtectedRoute>
+              <MarksPage />
+            </ProtectedRoute>
+          }
+        />
 
-            <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </MarksProvider>
   );
 }
